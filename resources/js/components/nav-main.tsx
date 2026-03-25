9695419815c9f1d28f1fe70a32bac64f59ee332d
Josh Donnell <joshua.donnell1@gmail.com>
2026-03-25 08:14:45 +0000

    Revert "fix: update to @vitejs/plugin-react v6 remove babel and add memoization"
    This reverts commit 63ec39a712a0aa868dbc91b21990f74e9b728ef5.

--- resources/js/components/nav-main.tsx
+++ resources/js/components/nav-main.tsx
@@ -1,5 +1,4 @@
 import { Link } from '@inertiajs/react';
-import { useMemo } from 'react';
 import {
     SidebarGroup,
     SidebarGroupLabel,
@@ -10,33 +9,26 @@ import {
 import { useCurrentUrl } from '@/hooks/use-current-url';
 import type { NavItem } from '@/types';
 
-function NavMainItem({ item }: { item: NavItem }) {
-    const { isCurrentUrl } = useCurrentUrl();
-    const tooltip = useMemo(() => ({ children: item.title }), [item.title]);
-
-    return (
-        <SidebarMenuItem>
-            <SidebarMenuButton
-                asChild
-                isActive={isCurrentUrl(item.href)}
-                tooltip={tooltip}
-            >
-                <Link href={item.href} prefetch>
-                    {item.icon && <item.icon />}
-                    <span>{item.title}</span>
-                </Link>
-            </SidebarMenuButton>
-        </SidebarMenuItem>
-    );
-}
-
 export function NavMain({ items = [] }: { items: NavItem[] }) {
+    const { isCurrentUrl } = useCurrentUrl();
+
     return (
         <SidebarGroup className="px-2 py-0">
             <SidebarGroupLabel>Platform</SidebarGroupLabel>
             <SidebarMenu>
                 {items.map((item) => (
-                    <NavMainItem key={item.title} item={item} />
+                    <SidebarMenuItem key={item.title}>
+                        <SidebarMenuButton
+                            asChild
+                            isActive={isCurrentUrl(item.href)}
+                            tooltip={{ children: item.title }}
+                        >
+                            <Link href={item.href} prefetch>
+                                {item.icon && <item.icon />}
+                                <span>{item.title}</span>
+                            </Link>
+                        </SidebarMenuButton>
+                    </SidebarMenuItem>
                 ))}
             </SidebarMenu>
         </SidebarGroup>
